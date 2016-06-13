36d9799f21f4a4fd511ef32d2544692afe949764
Vladimir Melnic <win32hack@gmail.com>
2016-06-13 15:25:41 +0300

    Added redis flush command.

---
+++ src/Command/RedisFlushCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Casebox\CoreBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Translation\MessageCatalogueInterface;
+use Symfony\Component\Translation\Translator;
+
+/**
+ * Class RedisFlushCommand
+ */
+class RedisFlushCommand extends ContainerAwareCommand
+{
+    /**
+     * Configure
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('casebox:redis:flush')
+            ->setDescription('Flush redis cache.');
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return null
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output = new SymfonyStyle($input, $output);
+        $this->getContainer()->get('casebox_core.service.redis_service')->flushAll();
+        $output->success("DONE!");
+    }
+}
