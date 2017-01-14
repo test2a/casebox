6adc50d7f5c12186b9306e6c1376aaecbf2d6388
Apprio Development <dstoudt@apprioinc.com>
2017-01-13 19:12:41 -0500

    Update tab reset

--- src/Resources/public/js/CB/browser/view/Dashboard.js
+++ src/Resources/public/js/CB/browser/view/Dashboard.js
@@ -11,7 +11,6 @@ Ext.define('CB.browser.view.Dashboard',{
     ,scrollable: true
 
     ,initComponent: function(){
-		Ext.state.Manager.set('active_tab', 0);
         Ext.apply(this, {
             title: L.Dashboard
             ,viewName: 'dashboard'
@@ -88,6 +87,7 @@ Ext.define('CB.browser.view.Dashboard',{
     }
 	
     ,onActivate: function() {
+	Ext.state.Manager.set('active_tab', 0);	    
         this.fireEvent(
             'settoolbaritems'
             ,[
